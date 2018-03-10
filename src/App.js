3aa44a3329e9ab5110edf50d5e45d142a11a2a53
Steven Heffner <steven.heffnersh@yahoo.com>
2018-03-09 20:22:10 -0700

    lay-out-but-part-fun

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
-import React, { Component } from 'react';
-import "./App.css"
+import React, { Component } from "react";
+import "./App.css";
+import Input from "./conponents/Input";
+import Header from "./conponents/Header"
+import axios from "axios"
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      dogImg: "",
+    }
+  this.getDog = this.getDog.bind(this)
+  this.randoDog = this.randoDog.bind(this)
+
+
+  }
+  getDog(name){
+    let promise = axios.get(`https://dog.ceo/api/breed/${name}/images/random`)
+    promise.then((res) => {
+      this.setState({dogImg: res.data.message})
+    })
+  }
+
+  randoDog(){
+    let promise = axios.get("https://dog.ceo/api/breeds/image/random")
+    promise.then((res) => {
+     this.setState({dogImg: res.data.message})
+    })
+  }
+  
+
+
+
   render() {
     return (
-      <div className="header">
-        <div className= "title">Pupper Liker</div>
-       </div> 
-      <div className="main-content">
+      <div className = "body">
+        <Header />
 
-      </div> 
+        <Input getDog= {this.getDog} dogImg={this.state.dogImg} randoDogFn={this.randoDog} dogTest = {this.dogTest} />
+
+
+
+
+
+
+      </div>
     );
   }
 }
